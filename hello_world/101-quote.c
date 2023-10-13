ab8ae49de469417b6352222ac4f666a6555ccf89
Luisru9 <lrgp42@outlook.com>
2023-10-13 13:23:57 -0400

    quote c.

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<unistd>
 /**
- * main - A c program that prints a line to standard error
- * Return 1 (Success)
+ * main - Prints exactly what is told
+ * Return: Always 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
 	return (1);
 }
