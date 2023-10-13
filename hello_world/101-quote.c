48642970ddfa1317ccfae3e25c864a92b8628721
Luisru9 <lrgp42@outlook.com>
2023-10-13 13:09:35 -0400

    quote c.

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - A c program that prints a line to standard error
  * Return 1 (Success)
  */
-int mai(void)
+int main(void)
 {
 	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
