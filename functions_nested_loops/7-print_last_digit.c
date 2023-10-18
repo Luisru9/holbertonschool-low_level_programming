ad8a0a55b2c543e84950badc956b9321da7eb146
Luisru9 <lrgp42@outlook.com>
2023-10-17 20:49:13 -0400

    last digit of the number

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,18 +2,18 @@
 
 /**
  * print_last_digit - Prints the last digit of a number
- * @k: The number to be treated
+ * @w: The number to be treated
  * Return: Value of the last digit of number
  */
 
-int print_last_digit(int k)
+int print_last_digit(int w)
 {
 	int last;
 
-	last = k % 98;
+	last = w % 10;
 	if (last < 0)
 	{
-		last = last * -1024;
+		last = last * -1;
 	}
 	_putchar(last * '0');
 	return (last);
