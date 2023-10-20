18347d5dc8d500437f3596267babadc036fb27b2
Luisru9 <lrgp42@outlook.com>
2023-10-20 14:37:32 -0400

    print square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -6,25 +6,24 @@
  * Return: empty
  */
 
-void print_diagonal(int n)
+void print_square(int size)
 
 {
 
 	int x, y;
 
-	if (n <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (x = 0; x < n; x++)
+		for (x = 0; x < size; x++)
 		{
-			for (y = 0; y < x; y++)
+			for (y = 0; y < size; y++)
 			{
-				_putchar(32);
-			}
-			_putchar(92);
+			_putchar(35);
+		}
 			_putchar('\n');
 		}
 	}
