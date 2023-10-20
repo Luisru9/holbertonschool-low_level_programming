9229e7f014c70e1fd17d8f4015e33e9cd50d3c15
Luisru9 <lrgp42@outlook.com>
2023-10-20 09:12:01 -0400

    isupper 1

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper-checl for uppercase character
+ * @c: function parameter
+ * Return: 1 on succes 0 on failure
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef main.h
+#define main.h
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+
+#endif
