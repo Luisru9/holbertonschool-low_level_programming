c8f2d1c7710b4e5a7ec143cfbb5033851df57f08
Luisru9 <lrgp42@outlook.com>
2023-10-20 14:44:30 -0400

    print square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,7 +2,7 @@
 
 /**
  * print_square - Draws a diagonal lines according parameter
- * @n: The number of times to print diagonal lines
+ * @size: The number of times to print diagonal lines
  * Return: empty
  */
 
