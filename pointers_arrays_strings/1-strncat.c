17cf75ff7f678538f23a8479b4b7000bc441e44c
Luisru9 <lrgp42@outlook.com>
2023-10-24 10:49:59 -0400

    conconates another 2 strings

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,23 @@
 #include "main.h"
+
+/**
+ * _strncat - Concanates tow strings using a most
+ * an imputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest
+ * @d: The number of bytes from src to be appended to dest.
+ * Return: a pointer to the resulting string dest.
+ */
+
+char *_strncat(char *dest, char *src, int d)
+
+{
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < d; index++)
+		dest[dest_lens++] = src[index];
+	return (dest);
+}
