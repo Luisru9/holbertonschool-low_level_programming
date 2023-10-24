df35ce0abdd48103b526a46360f1462d752aed07
Luisru9 <lrgp42@outlook.com>
2023-10-24 09:53:57 -0400

    concanates 2 strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,22 +8,22 @@
  * Return: A pointer to the resulting string dest
  */
 
-char *_strcat( char *dest, char *src)
+char *_strcat(char *dest, char *src)
 
 {
 	int dlen = 0, i;
 
-	while (dest [dlen])
+	while (dest[dlen])
 	{
 		dlen++;
 	}
 
-	for (i = 0; src [i] != 0; i++);
+	for (i = 0; src[i] != 0; i++)
 	{
-	dest[dlen] = src[i];
-	dlen++;
+		dest[dlen] = src[i];
+		dlen++;
 }
 
-dest [dlen] = '\0';
+dest[dlen] = '\0';
 return (dest);
 }
