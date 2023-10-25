16475e904b77241a38d11c41d9a41ebefec910c4
Luisru9 <lrgp42@outlook.com>
2023-10-25 11:23:36 -0400

    encoded string

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -10,8 +10,8 @@ char *leet(char *n)
 
 {
 	int i, j;
-	char s1[] -"aAeEoOtTlL";
-	char s2[] -"4433007711";
+	char n1[] -"aAeEoOtTlL";
+	char n2[] -"4433007711";
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
