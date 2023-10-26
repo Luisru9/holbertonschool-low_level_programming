8c37f10d8ae5655450e5074055109bb1dc6ba1ee
Luisru9 <lrgp42@outlook.com>
2023-10-26 08:33:36 -0400

    memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -8,7 +8,8 @@
  * Return: always 0 (success)
  */
 
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n
+		)
 {
 	unsigned int i;
 	for (i = 0; i < n; i++)
