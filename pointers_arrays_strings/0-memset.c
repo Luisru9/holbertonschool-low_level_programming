c2bea98a7dac954d732097d64bbdd562b334d3f3
Luisru9 <lrgp42@outlook.com>
2023-10-25 21:16:12 -0400

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - entry point
+ * @s: destination
+ * @b: constant
+ * @n: bytes
+ * Return: always 0 
+ */
+
+char *_memset(char *s, b, unsigned int n)
+{
+	unsigned int p;
+
+	for (p = 0; p < n; p++)
+		s[i] = b;
+	return (s);
+}
--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,26 +1,26 @@
 #include "main.h"
 /**
  * leet - a function that encodes a string into 1337.
- * @str: string to be encoded.
+ * @von: string to be encoded.
  * Return: a pointer of the string encoded.
  */
-char *leet(char *str)
+char *leet(char *von)
 {
-	char *tmp = str;
+	char *tmp = von;
 	char key[] = {'A', 'E', 'O', 'T', 'L'};
 	int value[] = {4, 3, 0, 7, 1};
 	unsigned int i;
 
-	while (*str)
+	while (*von)
 	{
 		for (i = 0; i < sizeof(key) / sizeof(char); i++)
 		{
-			if (*str == key[i] || *str == key[i] + 32)
+			if (*von == key[i] || *von == key[i] + 32)
 			{
-				*str = value[i] + '0';
+				*von = value[i] + '0';
 			}
 		}
-		str++;
+		von++;
 	}
 	return (tmp);
 }
