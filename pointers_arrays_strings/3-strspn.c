e79b099f27b6370c183e63fbd52a1434ba5327d4
Luisru9 <lrgp42@outlook.com>
2023-10-26 10:23:44 -0400

    prefix substring

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,19 +10,18 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int q, e, value, check;
+	int q, e, value, check;
 
-	value = 0;
+	check = 0;
 
-	for (q = 0; s[q] != '\0'; q++)
+	for (q = 0; s[q] > ' '; q++)
 	{
-		check = 0;
 
 		for (e = 0; accept[e] != '\0'; e++)
 		{
 			if (accept[e] == s[q])
 			{
-				check++
+				check++;
 			}
 		}
 	}
