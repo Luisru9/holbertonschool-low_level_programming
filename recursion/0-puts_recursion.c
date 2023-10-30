412aa1796b46bdf1de5eaa9008bce40f0f37eb0d
Luisru9 <lrgp42@outlook.com>
2023-10-30 09:23:37 -0400

    string new line

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string and a new line
+ * @s: string to print
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
