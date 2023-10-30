82162c174e6f8c2cbe257cff2840c84e100a5a27
Luisru9 <lrgp42@outlook.com>
2023-10-30 09:50:56 -0400

    reverse string

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -9,17 +9,13 @@
 void _print_rev_recursion(char *s)
 
 {
-
-	int len = 0, index = 0;
-	char tmp;
-
-	while (s[index++])
-		len++;
-	for (index = len -1; index >= len / 2; index--)
-	for (index = len - 1; index >= len / 2; index--)
+	if (*s == '\0')
 	{
-		tmp = s[index];
-		s[index] = s[len - index - 1];
-		s[len - index - 1] = tmp;
+		return;
 	}
+
+	s++;
+		_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
