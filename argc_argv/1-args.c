ee6580d444fe4354bd970f7b6676ae27f157660c
Luisru9 <lrgp42@outlook.com>
2023-10-31 19:16:53 -0400

    1 args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prog to print
+ * @argc: num
+ * @argv: names
+ * Return: 0
+ */
+
+int main(int argc, _attribute_((unused))char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
