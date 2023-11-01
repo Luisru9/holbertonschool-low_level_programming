14f4ffc32d675dce168c47a355de75c266131d33
Luisru9 <lrgp42@outlook.com>
2023-11-01 13:54:36 -0400

    args,argv

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - print the number of arguments passed to the program
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - entry point
+ * this finction prints char from first command
  *
- * Return: Always 0 (Success)
+ * @argc: number of command
+ * @argv: array of command
+ *
+ * Return: 0 on success
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 
 {
 	(void)argv;
-	printf("%d\n", argc - 1);
-	return (0);
+	printf("%d\n", argc - 1)
+		return (0);
 }
