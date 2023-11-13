4dc6080453e6bddccf740ed29f1d32fab60a5543
Luisru9 <lrgp42@outlook.com>
2023-11-13 18:14:04 -0400

    doggy work

---
+++ structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * struct dog - age owner name info
+ * @name: dog name
+ * @owner: owner name
+ * @age: dog age
+ *
+ */
+
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
+
+/**
+ * dog_t - typedef of dog info
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
