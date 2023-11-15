042b41625d25c966dcd974b38f84b9ab044b9118
Luisru9 <lrgp42@outlook.com>
2023-11-15 15:25:31 -0400

    dog info

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,7 +11,7 @@
  * Return: nothing
  */
 
-void init_dog(struct dog *d, char *name, floating age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 
 {
 	if (d != NULL)
