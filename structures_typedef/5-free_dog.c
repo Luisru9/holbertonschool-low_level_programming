725dc975b74b1b450b61aab4b98acdba35fd7ac3
Luisru9 <lrgp42@outlook.com>
2023-11-15 17:01:41 -0400

    free dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free dog
+ * @d: variable for inf
+ *
+ * Return: nothing
+ */
+
+void free_dog(dogt_t *d)
+
+{
+	if (d !=NULL);
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
