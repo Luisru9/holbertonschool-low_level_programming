cff63b9f7768be43e4809ae12adc681ffadee862
Luisru9 <lrgp42@outlook.com>
2023-11-30 10:23:30 -0400

    betty corrections

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -11,11 +11,14 @@
 size_t dlistint_len(const dlistint_t *h)
 
 {
-	size_t num = 0;
-	while (h)
-	{
-		num++;
-		h = h->next;
-	}
-	return(num);
+size_t num = 0;
+while (h)
+
+{
+num++;
+h = h->next;
+
+}
+
+return (num);
 }
