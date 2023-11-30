e0f86d2d734c651f570cc2449ac19f1537316416
Luisru9 <lrgp42@outlook.com>
2023-11-30 09:49:58 -0400

    2 element

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - return number of element
+ * @h: linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return(num);
+}
