9562946cd1dfa8eec4a3cc875c358da26d2ef5fe
Luisru9 <lrgp42@outlook.com>
2023-12-08 07:47:28 -0400

    appended text

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *append_text_to_file - append text to a file
+ *@filename: points to the file name
+ *@text_content: points to the txt content
+ *Return: ! if success -1 if fail
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+
+{
+	/*Declarations*/
+	int fd, len;
+	/*Check if the file exists*/
+	if (access(filename, F_OK) == -1)
+		return (-1);
+	/*check filename for content*/
+	if (filename == NULL)
+		return (-1);
+	/*Open the file*/
+	fd = open(filename, O_WRONLY | O_APPEND, 0664);
+	if (fd == -1)
+		return (-1);
+	/*check if tex_content is NULL or has data*/
+	if (text_content == NULL)
+	{
+		close(fd); /*if NULL, jsut close the file*/
+		return (1);
+	} /*end if*/
+	len = strlen(text_content);
+	write(fd, text_content, len);
+	close(fd);
+	return (1);
+} /*end function*/
