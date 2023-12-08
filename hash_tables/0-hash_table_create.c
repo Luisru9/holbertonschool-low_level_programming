ac662decd6e1f0f791d92cf237b0d3780cf0aa35
Luisru9 <lrgp42@outlook.com>
2023-12-08 12:32:24 -0400

    hash tables

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 
-	table->size = size
+	table->size = size;
 		table->array = malloc(sizeof(hash_node_t *) * table->size);
 
 	for (i = 0; i < table->size; i++)
