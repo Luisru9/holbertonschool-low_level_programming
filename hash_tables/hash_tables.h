725b1e91a07439761bf1c0a349531d016da24a15
Luisru9 <lrgp42@outlook.com>
2023-12-08 13:16:42 -0400

    hash tables

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -22,10 +22,17 @@ char *value;
 struct hash_node_s *next;
 } hash_node_t;
 
+/**
+ * struct hash_table_s - Node of a hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
+
 typedef struct hash_table_s
-
 {
-
 unsigned long int size;
 hash_node_t **array;
 } hash_table_t;
