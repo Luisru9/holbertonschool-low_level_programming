902068e3302f94a3d371872941203cb9d9414465
Luisru9 <lrgp42@outlook.com>
2023-12-09 15:08:09 -0400

    table set

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -6,46 +6,35 @@
 * @value: the value
 * Return: 1 on success, 0 otherwise
 */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+
 {
 	unsigned long int index;
-	hash_node_t *new = NULL, *temp = NULL;
+	hash_node_t *node;
 
-	if (key == NULL || key == '\0' || value == NULL || ht == NULL)
-	{
+	if (key == NULL || ht == NULL)
 		return (0);
-	}
-	index = key_index((const unsigned char *)key, (*ht).size);
-	temp = (*ht).array[index];
-	while (temp != NULL)
+
+	index = key_index((unsigned char *) key, ht->size);
+	node = ht->array[index];
+	while (node != NULL)
 	{
-		if (strcmp((*temp).key, key) == 0)
+		if (strcmp(node->key, key) == 0)
 		{
-			free((*temp).value);
-			(*temp).value = strdup(value);
+			free(node->value);
+			node->value = strdup(value);
 			return (1);
 		}
-		temp = (*temp).next;
+		node = node->next;
 	}
-	new = malloc(sizeof(hash_node_t));
-	if (!new)
-	{
+	node = malloc(sizeof(hash_table_t));
+	if (node == NULL)
 		return (0);
-	}
-	(*new).key = strdup(key);
-	if (!(*new).key)
-	{
-		free(new);
-		return (0);
-	}
-	(*new).value = strdup(value);
-	if (!(*new).value)
-	{
-		free((*new).key);
-		free(new);
-		return (0);
-	}
-	(*new).next = (*ht).array[index];
-	(*ht).array[index] = new;
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = ht->array[index];
+	ht->array[index] = node;
 	return (1);
 }
