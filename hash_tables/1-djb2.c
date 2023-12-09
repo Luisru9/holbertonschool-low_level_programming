5c73712783029cab83b2109a89be62f90300fd1a
Luisru9 <lrgp42@outlook.com>
2023-12-09 10:35:07 -0400

    dbj2 algorithm

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,20 +1,20 @@
 #include "hash_tables.h"
-
 /**
- * hash_djb2 - hash function algorith
- * @str: string input
- * Return: return hash info
+* hash_djb2 - hash function djb2 algorithm
+*
+* @str: string input
+* Return: returns hash
 */
 
-unsigned ling int hash_djb2(const unsigned char *str)
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
 
-{
-unsigned long int hash;
-int y;
-hash = 5381;
-while ((y = *str++))
-{
-hash = ((hash << 5) + hash) +y;
-}
-return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
