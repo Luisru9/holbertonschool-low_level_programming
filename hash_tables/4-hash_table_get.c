704aa0e87de6963e65f5a3cb555291629e71734c
Luisru9 <lrgp42@outlook.com>
2023-12-09 15:18:40 -0400

    table to get

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -23,7 +23,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
     {
         if (strcmp(tmp->key, key) == 0)
         return (tmp->value);
-        tmp = tm->mext;
+        tmp = tmp->next;
     }
     return (NULL);
 }
